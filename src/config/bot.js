7fb20199593434bd0ca2689d6fdec48d7992ae24
Eleabala <eleabala@myyahoo.com>
2026-05-01 04:15:50 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: ".gg/x66   .gg/T333",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
